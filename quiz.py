29fc9abcbaa56c2b97ffe643f9d114d0fcef8f0e
nurlight05 <nurlight05@gmail.com>
2022-06-12 16:49:59 +0500

    Letter labeled answers are added.

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+from string import ascii_lowercase
+
 QUESTIONS = {
     "When was the first known use of the word 'quiz'": [
         "1781", "1771", "1871", "1881"
@@ -22,16 +24,19 @@ QUESTIONS = {
     ],
 }
 
-for question, alternatives in QUESTIONS.items():
-    correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
     
-    for label, alternative in enumerate(sorted_alternatives):
+    correct_answer = alternatives[0]
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    
+    for label, alternative in labeled_alternatives.items():
         print(f" {label}) {alternative}")
     
-    answer_label = int(input(f"{question}? "))
-    answer = sorted_alternatives[answer_label]
+    answer_label = input("\nChoice? ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
-        print("Correct!")
+        print("⭐ Correct! ⭐")
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
