431bed1e3e4c35abfe08ce7659263bd13bfd8641
nurlight05 <nurlight05@gmail.com>
2022-06-12 19:33:36 +0500

    Changes .toml file data structure. Refactored quiz.py file.

--- quiz.py
+++ quiz.py
@@ -10,9 +10,35 @@ NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
 QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
 
-def prepare_questions(questions, num_questions):
+def run_quiz():
+    questions = prepare_questions(
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
+    
+    num_correct = 0
+    for num, question in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question)
+        
+    print(f"\nYou got {num_correct} correct out of {num} questions")
+
+def prepare_questions(path, num_questions):
+    questions = tomllib.loads(path.read_text())["questions"]
     num_questions = min(num_questions, len(questions))
-    return random.sample(list(questions.items()), k=num_questions)
+    return random.sample(questions, k=num_questions)
+
+def ask_question(question):
+    correct_answer = question["answer"]
+    alternatives = [question["answer"]] + question["alternatives"]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+    
+    answer = get_answer(question["question"], ordered_alternatives)
+    if answer == correct_answer:
+        print("⭐ Correct! ⭐")
+        return 1
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
+        return 0
 
 def get_answer(question, alternatives):
     print(f"{question}?")
@@ -26,29 +52,5 @@ def get_answer(question, alternatives):
         
     return labeled_alternatives[answer_label]
 
-def ask_question(question, alternatives):
-    correct_answer = alternatives[0]
-    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
-    
-    answer = get_answer(question, ordered_alternatives)
-    if answer == correct_answer:
-        print("⭐ Correct! ⭐")
-        return 1
-    else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")
-        return 0
-    
-def run_quiz():
-    questions = prepare_questions(
-        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
-    )
-    
-    num_correct = 0
-    for num, (question, alternatives) in enumerate(questions, start=1):
-        print(f"\nQuestion {num}:")
-        num_correct += ask_question(question, alternatives)
-        
-    print(f"\nYou got {num_correct} correct out of {num} questions")
-    
 if __name__ == "__main__":
     run_quiz()
